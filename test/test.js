18fb9f6cd8fcbb6328b189ec5b2ce64c3c5b6e25
Gennaro Rimola <oeffentlich.gr@gmx.de>
2020-10-20 19:19:36 +0200

    Project updated

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
-description('Array',function () {
-    description('#indexOf()',function () {
-        if('should return -1 when the value is not present', function () {
-            assert.equal(-1,[1,2,3].indexOf(4));
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });
 });
