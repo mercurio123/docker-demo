99c27a2029ef4ebce5556741b696f90d37cdc763
Gennaro Rimola <oeffentlich.gr@gmx.de>
2020-10-21 13:37:30 +0200

    Project updated

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var mocha = require('mocha');
+var describe = mocha.describe;
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
